b7375b299c361099790c6741076ce2d0fe05c290
dperezch <davarmando@gmail.com>
2023-04-04 20:45:20 -0400

    validadores

--- components/CrearUsuario.js
+++ components/CrearUsuario.js
@@ -1,41 +1,75 @@
 import { db } from "@/firebase";
-import { Button, TextField } from "@mui/material";
-import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import { useEffect, useRef, useState } from "react";
+import { ContextUsuario } from "@/pages/ContextUsuario";
+import { Button, TextField, Tooltip } from "@mui/material";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
+import { useContext, useEffect, useRef } from "react";
 
 const CrearUsuario = () => {
-  const [usuario, setUsuario] = useState({
+  /* const [usuario, setUsuario] = useState({
     nombre: "",
     email: "",
     telefono: "",
-  });
+  }); */
+
+  //Provider
+  const { usuario, setUsuario } = useContext(ContextUsuario);
 
   const areaEntrada = useRef();
 
   useEffect(() => {
-    const validarClickAfuera = e => {
-        if (!areaEntrada.current.contains(e.target)) {
-            console.log("Area de entrada afuera");
-            setUsuario({nombre: '', email: '', telefono: ''})
-        } else {
-            console.log("Area de entrada adentro");
-        }   
-    }
-    document.addEventListener('mousedown', validarClickAfuera)
-  
+    const validarClickAfuera = (e) => {
+      if (!areaEntrada.current.contains(e.target)) {
+        console.log("Area de entrada afuera");
+        setUsuario({ nombre: "", email: "", telefono: "" });
+      } else {
+        console.log("Area de entrada adentro");
+      }
+    };
+    document.addEventListener("mousedown", validarClickAfuera);
+
     return () => {
-        document.removeEventListener('mousedown', validarClickAfuera)
-    }
-  }, [])
-  
+      document.removeEventListener("mousedown", validarClickAfuera);
+    };
+  }, []);
 
   const onSubmit = async () => {
-    //crear usuario
-    const coleccion = collection(db, "usuarios")
-    const documento = await addDoc(coleccion, {...usuario, fecha: serverTimestamp()})
-    setUsuario({nombre:'', email: '', telefono: ''})
-    alert(`Se ha creado correctamente el usuario con el ID: ${documento.id}`)
-  }
+    if (usuario?.hasOwnProperty("fecha")) {
+      //si tiene la propiedad
+      //Actualizar
+      const documento = doc(db, "usuarios", usuario.id);
+      const usuarioActualizado = { ...usuario, fecha: serverTimestamp() };
+      updateDoc(documento, usuarioActualizado);
+      setUsuario({ nombre: "", email: "", telefono: "" });
+      alert(
+        `El Usuario con el ID: ${documento.id} se ha actualizado correctamente`
+      );
+    } else {
+      //crear usuario
+      const coleccion = collection(db, "usuarios");
+      const documento = await addDoc(coleccion, {
+        ...usuario,
+        fecha: serverTimestamp(),
+      });
+      setUsuario({ nombre: "", email: "", telefono: "" });
+      alert(`Se ha creado correctamente el usuario con el ID: ${documento.id}`);
+    }
+  };
+
+  const validateEmail = (email) => {
+     if( String(email).match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    )){
+      return false
+    } else {
+      return true
+    }
+  };
 
   return (
     <div ref={areaEntrada}>
@@ -47,29 +81,60 @@ const CrearUsuario = () => {
         onChange={(e) => setUsuario({ ...usuario, nombre: e.target.value })}
       ></TextField>
       <TextField
+        error={usuario.email.match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        ) || usuario.email === '' ? false : true}
+        helperText={usuario.email.match(
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        )? "Formato correcto" : "formato incorrecto"}
         fullWidth
         label="email"
         margin="normal"
         value={usuario.email}
-        onChange={(e) => setUsuario({ ...usuario, email: e.target.value })}
+        autoComplete="email"   //...usuario, email: e.target.value
+        onChange={(e) => setUsuario({ ...usuario, email: e.target.value })}  
       ></TextField>
       <TextField
         fullWidth
         label="telefono"
         margin="normal"
+        type="number"
         value={usuario.telefono}
         onChange={(e) => setUsuario({ ...usuario, telefono: e.target.value })}
       ></TextField>
 
-      <Button 
-      fullWidth 
-      variant="contained" 
-      color="secondary" 
-      mx={{ mt: 3 }}
-      onClick={onSubmit}
+      <Tooltip
+        title={
+          usuario.nombre === "" ||
+          usuario.email === "" ||
+          usuario.telefono === ""
+            ? "completa todos los campos"
+            : ""
+        }
       >
-        Crear usuario
-      </Button>
+        <span>
+          <Button
+            fullWidth
+            variant="contained"
+            color="secondary"
+            mx={{ mt: 3 }}
+            onClick={onSubmit}
+            disabled={
+              usuario.nombre === "" ||
+              usuario.email === "" ||
+              usuario.telefono === "" || !usuario.email.match(
+                /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+              )
+                ? true
+                : false
+            }
+          >
+            {usuario.hasOwnProperty("fecha")
+              ? "Actualizar Usuario"
+              : "Crear Usuario"}
+          </Button>
+        </span>
+      </Tooltip>
     </div>
   );
 };
--- components/Usuario.js
+++ components/Usuario.js
@@ -1,33 +1,50 @@
-import { IconButton, ListItem, ListItemText } from "@mui/material"
-import EditIcon from '@mui/icons-material/Edit'
-import DeleteIcon from '@mui/icons-material/Delete'
-import moment from "moment/moment"
+import { IconButton, ListItem, ListItemText } from "@mui/material";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import moment from "moment/moment";
+import { useContext } from "react";
+import { ContextUsuario } from "@/pages/ContextUsuario";
+import { db } from "@/firebase";
+import { deleteDoc, doc } from "firebase/firestore";
 
+const Usuario = ({ id, nombre, email, telefono, fecha }) => {
+  //Provider
+  const { usuario, setUsuario } = useContext(ContextUsuario);
 
-const Usuario = ({id, nombre, email, telefono, fecha}) => {
+  //Borrar usuario
+  const borrarUsuario = async(id, e) =>{
+    e.stopPropagation()
+    const documento = doc(db, "usuarios", id);
+    await deleteDoc(documento)
+    alert(`El usuario con el ID: ${documento.id} se ha borrado correctamente`)
+  }
+
   return (
     <ListItem
-        sx={{mt: 3, boxShadow: 5}}
-        style={{backgroundColor: '#2196f3', color: '#fff'}}
-        secondaryAction={
-            <>
-                <IconButton> <EditIcon /> </IconButton>
-                <IconButton> <DeleteIcon /> </IconButton>
-            </>
-        }
+      sx={{ mt: 3, boxShadow: 5 }}
+      style={{ backgroundColor: "#2196f3", color: "#fff" }}
+      secondaryAction={
+        <>
+          <IconButton
+            onClick={() => setUsuario({ id, nombre, email, telefono, fecha })}
+          >
+            <EditIcon />
+          </IconButton>
+          <IconButton
+            onClick={e => borrarUsuario( id, e )}
+          >
+            <DeleteIcon />
+          </IconButton>
+        </>
+      }
     >
-        <ListItemText
-            primary={nombre}
-            secondary={email}
-        >
-        </ListItemText>
-        <ListItemText
-            primary={telefono}
-            secondary={moment(fecha).format("MMMM dd, yyyy")}
-        >
-        </ListItemText>
+      <ListItemText primary={nombre} secondary={email}></ListItemText>
+      <ListItemText
+        primary={telefono}
+        secondary={moment(fecha).format("MMMM dd, yyyy")}
+      ></ListItemText>
     </ListItem>
-  )
-}
+  );
+};
 
-export default Usuario
+export default Usuario;
